d01f433c3358cbfda66ea5deb581579dc663e4f9
k1rnt <eru08tera15mora@gmail.com>
2021-10-24 00:49:23 +0900

    add brew.sh

---
+++ macos/intel/brew.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+if [ "$(uname -s)" != "Darwin" ]; then
+	exit 0
+fi
+
+set -eu
+
+# workdir
+cd $HOME
+
+# homebrew
+## install
+which brew >/dev/null 2>&1 || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+## update & upgrade
+brew update && brew upgrade
+
+## install formula
+formulas=(
+    bat
+    ctop
+    exa
+    ffmpeg
+    fd
+    fish
+    gh
+    git
+    htop
+    mas
+    nb
+    nkf
+    neofetch
+    pandoc
+    procs
+    pstree
+    sd
+    thefuck
+    tldr
+    tmux
+    tree
+)
+
+for formula in "${formulas[@]}"; do
+    brew install $formula || brew upgrade $formula
+done
+
+## install casks
+casks=(
+    appcleaner
+    deepl
+    discord
+    docker
+    filezilla
+    firefox
+    google-chrome
+    google-japanese-ime
+    iterm2
+    slack
+    visual-studio-code
+)
+
+for cask in "${casks[@]}"; do
+    brew install --cask $cask
+done
+
+## complate
+brew cleanup
