a2223cf1bf5e847e8aa207c0985c459b281a8434
k1rnt <eru08tera15mora@gmail.com>
2021-10-23 23:38:22 +0900

    add settings and reflect

--- macos/intel/setup.sh
+++ macos/intel/setup.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
+if [ "$(uname -s)" != "Darwin" ]; then
+	exit 0
+fi
 
 set -eu
+
+sudo -v
 
 # workdir
 cd $HOME
@@ -14,7 +19,6 @@ defaults write com.apple.dock orientation -string "left"
 defaults write com.apple.dock launchanim -bool false
 defaults write com.apple.dock show-recents -bool false
 defaults write com.apple.dock static-only -boolean true
-killall Dock
 
 ## trackpad
 defaults write -g com.apple.mouse.tapBehavior -int 1 
@@ -27,13 +31,34 @@ defaults write com.apple.finder _FXSortFoldersFirst -bool true
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+chflags nohidden ~/Library
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 
 ## window
 defaults write -g NSAutomaticWindowAnimationsEnabled -bool false
-defaults write -g NSWindowResizeTime 0.1
+defaults write NSGlobalDomain NSWindowResizeTime -float 0.001
+
+## keyboard
+defaults write NSGlobalDomain KeyRepeat -int 2
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
+defaults write -g ApplePressAndHoldEnabled -bool false
 
 ## other
 defaults write -g NSAutomaticSpellingCorrectionEnabled -bool false
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
+defaults write NSGlobalDomain AppleInterfaceStyle -string "Dark"
+defaults write NSGlobalDomain AppleAquaColorVariant -int 6
+defaults write NSGlobalDomain AppleHighlightColor -string "0.847059 0.847059 0.862745"
+defaults write com.apple.menuextra.battery ShowPercent -bool true
+
+## Reflect
+for app in "Activity Monitor" "cfprefsd" \
+	"Dock" "Finder" "Safari" "SystemUIServer" \
+	"Terminal"; do
+	killall "$app" >/dev/null 2>&1
+done
 
 ## xcode
 xcode-select --install
